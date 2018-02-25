0385297a5dfcd204af3d954014c45ce61d78454e
whybandre <brunorfandre@gmail.com>
2018-02-25 11:52:31 +0000

    feat: style
    Added commeents and suggestions
    Java code update

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -19,20 +19,42 @@ public class MainActivity extends AppCompatActivity {
     static final String savedShotTeamB = "shotTeamB";
     static final String savedFoulsTeamA = "foulsTeamA";
     static final String savedFoulsTeamB = "foulsTeamB";
+
+
+    /**SUGGESTION from a Review
+     * These fields should be private to favour encapsulation.
+     * Software objects are conceptually similar to real-world objects:
+     * they too consist of state and related behavior.
+     * An object stores its state in fields (variables in some programming languages) and
+     * exposes its behavior through methods (functions in some programming languages).
+     * Methods operate on an object's internal state and serve as the primary mechanism for object-to-object communication.
+     * Hiding internal state and requiring all interaction to be performed through an object's methods is
+     * known as data encapsulation — a fundamental principle of object-oriented programming.
+     *You can read more about encapsulation here: https://www.tutorialspoint.com/java/java_encapsulation.htm
+     */
+
+
+    /**
+     * SUGGESTION from a Review
+     * int scoreTeamA = 0; --------> int scoreTeamA
+     * -->Int are zero by default<--, there is no need to re-initialize them
+     */
+
+
     // Tracking Score for Team A and B
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
+    private int scoreTeamA;
+    private int scoreTeamB;
     // Tracking Yellow Cards for Team A and B
-    int yellowCardTeamA = 0;
-    int yellowCardTeamB = 0;
+    private int yellowCardTeamA;
+    private int yellowCardTeamB;
     // Tracking Red Cards for Team A and B
-    int redCardTeamA = 0;
-    int redCardTeamB = 0;
-    int shotTeamA = 0;
-    int shotTeamB = 0;
+    private int redCardTeamA;
+    private int redCardTeamB;
+    private int shotTeamA;
+    private int shotTeamB;
     // Tracking Fouls for Team A and B
-    int foulsTeamA = 0;
-    int foulsTeamB = 0;
+    private int foulsTeamA;
+    private int foulsTeamB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * @param savedInstanceState Saving Variables
-     *                           More reading https://developer.android.com/guide/components/activities/activity-lifecycle.html
+     * More reading https://developer.android.com/guide/components/activities/activity-lifecycle.html
      */
     public void onSaveInstanceState(Bundle savedInstanceState) {
         //Saving Score
@@ -117,6 +139,16 @@ public class MainActivity extends AppCompatActivity {
 
     //TEAM A
 
+    /**SUGGESTION from a Review
+     * This is a bit of a more advanced suggestion.
+     * For all these methods called on the click of a view, the framework passes the view being clicked in the parameter
+     * v which you are never using. The reason why the view is being passed in, it is because you do not need
+     * to define a method for every ui element being clicked, as you can just check the id of the view being passed as
+     * a parameter to understand which button was clicked and increment the right score. In that case, you only need one of
+     * these methods, which avoid code duplication and simplify readability.
+     */
+
+
     /**
      * Score board increase by 1 unit in Team A.
      */
@@ -148,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the shots stats for Team A.
      */
     public void displayShotsForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.shot_card_team_a);
+        TextView scoreView = findViewById(R.id.shot_card_team_a);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -203,7 +235,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the shots stats for Team B.
      */
     public void displayShotsForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.shot_card_team_b);
+        TextView scoreView = findViewById(R.id.shot_card_team_b);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -233,10 +265,10 @@ public class MainActivity extends AppCompatActivity {
 
         //Takes the text name of team and save it has a string
 
-        EditText editNameTeamA = (EditText) findViewById(R.id.edit_team_a);
+        EditText editNameTeamA = findViewById(R.id.edit_team_a);
         String nameTeamA = editNameTeamA.getText().toString();
 
-        EditText editNameTeamB = (EditText) findViewById(R.id.edit_team_b);
+        EditText editNameTeamB = findViewById(R.id.edit_team_b);
         String nameTeamB = editNameTeamB.getText().toString();
 
         //Display text according to result
@@ -303,7 +335,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreView = findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -311,7 +343,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the yellow cards for Team A.
      */
     public void displayYellowCardForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.yellow_card_team_a);
+        TextView scoreView = findViewById(R.id.yellow_card_team_a);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -319,7 +351,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the red cards for Team A.
      */
     public void displayRedCardForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.red_card_team_a);
+        TextView scoreView = findViewById(R.id.red_card_team_a);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -327,7 +359,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the fouls stats for Team A.
      */
     public void displayFoulsForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.fouls_team_a);
+        TextView scoreView = findViewById(R.id.fouls_team_a);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -338,7 +370,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Team B.
      */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        TextView scoreView = findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -347,7 +379,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the yellow cards for Team B.
      */
     public void displayYellowCardForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.yellow_card_team_b);
+        TextView scoreView = findViewById(R.id.yellow_card_team_b);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -355,7 +387,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the red cards for Team B.
      */
     public void displayRedCardForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.red_card_team_b);
+        TextView scoreView = findViewById(R.id.red_card_team_b);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -363,7 +395,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the fouls stats for Team B.
      */
     public void displayFoulsForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.fouls_team_b);
+        TextView scoreView = findViewById(R.id.fouls_team_b);
         scoreView.setText(String.valueOf(score));
     }
 
